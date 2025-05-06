e69e1a5a9bc61f748597eb9f0cd589d43b6a3fb6
shravanigarje <shravanigarje15@gmail.com>
2025-05-06 16:24:29 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,26 +1,32 @@
+import logo from './logo.svg';
 import './App.css';
+import Greet from './component/Greet';
+import ClassEx from './component/ClassEx';
+import ClassLife from './component/ClassLife';
+import State from './component/State';
+import StateEx1 from './component/StateEx1';
+import PropsEx from './component/PropsEx';
+import JSXEx from './component/JSXEx';
+import JSXFunc from './component/JSXFunc';
+import JSXattr from './component/JSXattr';
+import JSXchild from './component/JSXchild';
 
-import ControlForm from "./component/ControlForm";
-import ControlForm1 from "./component/ControlForm1";
-import UncontrolForm from "./component/UncontrolForm";
-import UncontrolForm1 from "./component/UncontrolForm1";
-import MemoHook from "./component/MemoHook";
 function App() {
+  const data={name:"Shravani"};
   return (
     <div className="App">
       <header className="App-header">
-      <h3>Controlled Component</h3>
-      <ControlForm />
-       <h4>Controlled Component using func</h4>
-      <ControlForm1 />
-       <h3>UnControlled Component</h3>
-      <UncontrolForm />
-       <h4>UnControlled Component using class</h4>
-      <UncontrolForm1 />
-      <counter />
-
-      <MemoHook />
-      
+  
+      <Greet />
+      <JSXchild />
+      <ClassEx />
+      <ClassLife />
+      <State />
+      <StateEx1 />
+      <PropsEx val={data} />
+      <JSXEx />
+      <JSXFunc />
+      <JSXattr />
       </header>
     </div>
   );
